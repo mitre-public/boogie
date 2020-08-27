12b3024c45e744129304ef0edef278adc14a5a79
Alex <acramer@mitre.org>
2020-08-27 11:42:37 -0400

    update ci script branch names

--- ci-scripts/ci-publish-snapshot.sh
+++ ci-scripts/ci-publish-snapshot.sh
@@ -10,11 +10,11 @@ branchName=$(git rev-parse --abbrev-ref HEAD)
 myProjVersion=$(./gradlew properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
 echo "Running ${BLUE}$0${NONE} on branch ${BLUE}$branchName${NONE} at version: ${BLUE}$myProjVersion${NONE}"
 
-# only publish for this build if SNAPSHOT version AND on master branch
-if [[ "$myProjVersion" == *SNAPSHOT ]] && [ $branchName = "master" ]; then
-  echo "${GREEN}publishing a master SNAPSHOT...$NONE"
+# only publish for this build if SNAPSHOT version AND on main branch
+if [[ "$myProjVersion" == *SNAPSHOT ]] && [ $branchName = "main" ]; then
+  echo "${GREEN}publishing a main SNAPSHOT...$NONE"
   output=$(./gradlew publish $1 $2)
   echo "$output"
 else
-  echo "not on ${YELLOW}master${NONE} (${BLUE}$branchName${NONE}) branch or not on a ${YELLOW}SNAPSHOT${NONE} (${BLUE}$myProjVersion${NONE}) version.. ${RED}NOT PUBLISHING!${NONE}"
+  echo "not on ${YELLOW}main${NONE} (${BLUE}$branchName${NONE}) branch or not on a ${YELLOW}SNAPSHOT${NONE} (${BLUE}$myProjVersion${NONE}) version.. ${RED}NOT PUBLISHING!${NONE}"
 fi
