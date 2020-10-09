314ae3d32d5b5627f2125b782ae9d24686763a04
madelfio <madelfio@mitre.org>
2020-10-09 12:24:21 -0400

    TDP-4389 Use Rec Navaid for AF leg off-track distance computation

--- boogie-conformance/src/test/java/org/mitre/tdp/boogie/conformance/alg/evaluate/TestMaxOffTrackDistanceEvaluator.java
+++ boogie-conformance/src/test/java/org/mitre/tdp/boogie/conformance/alg/evaluate/TestMaxOffTrackDistanceEvaluator.java
@@ -19,28 +19,43 @@ import org.mockito.stubbing.Answer;
 
 class TestMaxOffTrackDistanceEvaluator {
   @Test
-  void LegDistance_OnLeg_IsRadialDistance() {
+  void LegDistance_OnRfLeg_IsRadialDistance() {
     ConformablePoint pt = mock(ConformablePoint.class, withSettings().defaultAnswer(CALLS_REAL_METHODS));
     when(pt.latLong()).thenReturn(LatLong.of(0.0, 2.0));
-    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockLegPair()).orElseThrow(() -> new IllegalStateException());
+    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockRfLegPair()).orElseThrow(() -> new IllegalStateException());
     assertEquals(60., dist.inNauticalMiles(), 0.01);
   }
 
   @Test
-  void LegDistance_OffLeg_IsEndpointDistance() {
+  void LegDistance_OffRfLeg_IsEndpointDistance() {
     ConformablePoint pt = mock(ConformablePoint.class, withSettings().defaultAnswer(CALLS_REAL_METHODS));
     when(pt.latLong()).thenReturn(LatLong.of(0.0, -1.0));
-    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockLegPair()).orElseThrow(() -> new IllegalStateException());
+    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockRfLegPair()).orElseThrow(() -> new IllegalStateException());
     assertEquals(60. * Math.sqrt(2), dist.inNauticalMiles(), 0.01);
   }
 
-  static LegPair mockLegPair() {
+  @Test
+  void LegDistance_OnAfLeg_IsRadialDistance() {
+    ConformablePoint pt = mock(ConformablePoint.class, withSettings().defaultAnswer(CALLS_REAL_METHODS));
+    when(pt.latLong()).thenReturn(LatLong.of(0.0, 2.0));
+    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockAfLegPair()).orElseThrow(() -> new IllegalStateException());
+    assertEquals(60., dist.inNauticalMiles(), 0.01);
+  }
+
+  @Test
+  void LegDistance_OffAfLeg_IsEndpointDistance() {
+    ConformablePoint pt = mock(ConformablePoint.class, withSettings().defaultAnswer(CALLS_REAL_METHODS));
+    when(pt.latLong()).thenReturn(LatLong.of(0.0, -1.0));
+    Distance dist = MaxOffTrackDistanceEvaluator.offTrackDistance(pt, mockAfLegPair()).orElseThrow(() -> new IllegalStateException());
+    assertEquals(60. * Math.sqrt(2), dist.inNauticalMiles(), 0.01);
+  }
+
+  static LegPair mockRfLegPair() {
     Fix previousPathTerminator = mock(Fix.class, "PREVIOUS");
     Fix currentPathTerminator = mock(Fix.class, "CURRENT");
     Fix centerFix = mock(Fix.class,"CENTER");
     Leg previousLeg = mock(Leg.class);
     Leg currentLeg = mock(Leg.class);
-    LegPair legPair = new LegPairImpl(previousLeg, currentLeg);
 
     when(previousLeg.pathTerminator()).thenReturn(previousPathTerminator);
     when(currentLeg.pathTerminator()).thenReturn(currentPathTerminator);
@@ -54,6 +69,31 @@ class TestMaxOffTrackDistanceEvaluator {
     when(currentLeg.type()).thenReturn(PathTerm.RF);
     when(currentLeg.turnDirection()).then((Answer<Optional<TurnDirection>>) (x -> Optional.of(TurnDirection.left())));
     when(centerFix.latLong()).thenReturn(LatLong.of(0., 0.));
+    when(previousPathTerminator.latLong()).thenReturn(LatLong.of(0.0, 1.0));
+    when(currentPathTerminator.latLong()).thenReturn(LatLong.of(1.0, 0.0));
+
+    return new LegPairImpl(previousLeg, currentLeg);
+  }
+
+  static LegPair mockAfLegPair() {
+    Fix previousPathTerminator = mock(Fix.class, "PREVIOUS");
+    Fix currentPathTerminator = mock(Fix.class, "CURRENT");
+    Fix recommendedNavaid = mock(Fix.class,"CENTER");
+    Leg previousLeg = mock(Leg.class);
+    Leg currentLeg = mock(Leg.class);
+
+    when(previousLeg.pathTerminator()).thenReturn(previousPathTerminator);
+    when(currentLeg.pathTerminator()).thenReturn(currentPathTerminator);
+    when(currentLeg.recommendedNavaid()).then((Answer<Optional<Fix>>) (x -> Optional.of(recommendedNavaid)));
+
+    when(previousPathTerminator.distanceInNmTo(any())).thenCallRealMethod();
+    when(currentPathTerminator.distanceInNmTo(any())).thenCallRealMethod();
+    when(recommendedNavaid.distanceInNmTo(any())).thenCallRealMethod();
+    when(recommendedNavaid.courseInDegrees(any())).thenCallRealMethod();
+
+    when(currentLeg.type()).thenReturn(PathTerm.AF);
+    when(currentLeg.turnDirection()).then((Answer<Optional<TurnDirection>>) (x -> Optional.of(TurnDirection.left())));
+    when(recommendedNavaid.latLong()).thenReturn(LatLong.of(0., 0.));
     when(previousPathTerminator.latLong()).thenReturn(LatLong.of(0.0, 1.0));
     when(currentPathTerminator.latLong()).thenReturn(LatLong.of(1.0, 0.0));
 
