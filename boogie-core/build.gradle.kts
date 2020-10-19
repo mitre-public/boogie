01ff1a9280cec474f4d4e68dd2d0f17b266b6d2c
Alex <acramer@mitre.org>
2020-10-19 10:00:03 -0400

    exculude transitive guava deps via rtree from commons

--- boogie-core/build.gradle.kts
+++ boogie-core/build.gradle.kts
@@ -1,5 +1,9 @@
 dependencies {
-    api("org.mitre.caasd:commons:0.0.27")
+    api("org.mitre.caasd:commons:0.0.27") {
+        exclude("com.github.davidmoten", "rtree")
+        exclude("com.github.davidmoten", "guava-mini")
+        exclude("com.github.davidmoten", "rxjava-extras")
+    }
     implementation("com.google.guava:guava:23.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.4.2")
