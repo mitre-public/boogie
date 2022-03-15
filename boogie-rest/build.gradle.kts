287af468ba9eaac16882ceade55558ab7ca197fe
Alex <acramer@mitre.org>
2022-03-15 11:56:45 -0400

    Bump to TT-0.0.12 to catch open file handle fix

--- boogie-rest/build.gradle.kts
+++ boogie-rest/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     api(project(":boogie-arinc"))
     api(project(":boogie-routes"))
 
-    implementation("org.mitre.tdp:tentacular-core:0.0.11")
+    implementation("org.mitre.tdp:tentacular-core:0.0.12")
 
     /** Spring dependency to host a REST API */
     implementation("org.springframework.boot:spring-boot-starter-data-rest") {
