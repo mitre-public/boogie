ccde508667399c896ec0db329c175a5a6f6520d1
David Baker <dbaker@mitre.org>
2025-04-02 14:08:39 -0400

    publishing plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,18 @@ plugins {
     `kotlin-dsl`
 }
 
+val ghprToken: String? by project
+
 repositories {
     gradlePluginPortal()
+    configureGitHubRepo("mitre-public", "*")
+}
+
+fun RepositoryHandler.configureGitHubRepo(org: String, repo: String) {
+    maven {
+        name = "${org}-${repo}"
+        url = uri("https://maven.pkg.github.com/${org}/${repo}")
+        credentials(HttpHeaderCredentials::class) { name = "Authorization"; value = "Bearer ${ghprToken}" }
+        authentication { create<HttpHeaderAuthentication>("header") }
+    }
 }
