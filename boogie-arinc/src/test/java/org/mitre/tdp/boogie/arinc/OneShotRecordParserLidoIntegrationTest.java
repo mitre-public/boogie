a4dea05c5b1d132201e447f9e51507adab18b6ec
David Baker <dbaker@mitre.org>
2025-04-07 10:33:33 -0400

    test tags

--- boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/OneShotRecordParserLidoIntegrationTest.java
+++ boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/OneShotRecordParserLidoIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mitre.caasd.commons.util.DemotedException;
 import org.mitre.tdp.boogie.*;
 
+@Tag("LIDO")
 @Tag("INTEGRATION")
 public class OneShotRecordParserLidoIntegrationTest {
   @Test
--- boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/OneshotRecordParserCifpIntegrationTest.java
+++ boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/OneshotRecordParserCifpIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mitre.caasd.commons.util.DemotedException;
 import org.mitre.tdp.boogie.*;
 
+@Tag("CIFP")
 @Tag("INTEGRATION")
 class OneshotRecordParserCifpIntegrationTest {
 
