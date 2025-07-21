22004f3de5fd43908feb682274ca677c5d5a0b6c
David Baker <dbaker@mitre.org>
2025-07-21 09:33:53 -0400

    Found missing javadocs while i was using the models.

--- boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/StationElevationWgs84.java
+++ boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/StationElevationWgs84.java
@@ -2,6 +2,10 @@ package org.mitre.tdp.boogie.arinc.v18.field;
 
 import org.mitre.tdp.boogie.arinc.ArincDouble;
 
+/**
+ * The value for this field will be derived from official government
+ * sources or entered into this field in feet with respect to the WGS84 ellipsoid.
+ */
 public final class StationElevationWgs84 extends ArincDouble {
   @Override
   public int fieldLength() {
