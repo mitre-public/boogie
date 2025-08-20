dc25d5be4bf45eb768de1f04785883ee1944ea94
David Baker <76011631+dbaker-mitre@users.noreply.github.com>
2025-08-20 10:12:11 -0400

    Found missing javadocs while i was using the models. (#35)
    fix: minor doc update

--- boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/TdmaSlots.java
+++ boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/TdmaSlots.java
@@ -2,6 +2,18 @@ package org.mitre.tdp.boogie.arinc.v18.field;
 
 import org.mitre.tdp.boogie.arinc.TrimmableString;
 
+/**
+ * The Time Division Multiple Access (TDMA) identifies the time
+ * slot(s) in which the ground station transmits the related approach. The high
+ * precision time source available through GPS permits utilization of Time division
+ * multiplexing or TDMA (Time Division Multiple Access), allowing multiple ground
+ * stations to share a common frequency by dividing it into eight time slots. An
+ * individual station may broadcast in one or more of eight slots.
+ * <br>
+ * The value for this field will be derived from official government
+ * sources. The range is 01 to FF. If no source is provided, the default value will be
+ * blank.
+ */
 public final class TdmaSlots extends TrimmableString {
   @Override
   public int fieldLength() {
