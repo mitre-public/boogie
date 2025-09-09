9781cc1601743f0034c2d5c8aa254649d96bd3c9
David Baker <dbaker@mitre.org>
2025-09-09 13:09:52 -0400

    cleanup

--- boogie-routes/src/main/java/org/mitre/tdp/boogie/alg/facade/InarticulateLegConverter.java
+++ boogie-routes/src/main/java/org/mitre/tdp/boogie/alg/facade/InarticulateLegConverter.java
@@ -5,6 +5,9 @@ import java.util.function.Function;
 
 import org.mitre.tdp.boogie.alg.ResolvedLeg;
 
+/**
+ * This class is only converts the resolved legs to expanded route legs. No changing or combining.
+ */
 final class InarticulateLegConverter implements Function<List<ResolvedLeg>, List<ExpandedRouteLeg>> {
   private final ResolvedLegConverter  resolvedLegConverter = new ResolvedLegConverter();
 
