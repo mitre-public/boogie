aaeeabc115e39ddd0562f1e838bdd98372e1548f
Matthew Pollock <mpollock@mitre.org>
2025-09-12 22:06:07 -0400

    deploy all the modules

--- boogie-dafif/build.gradle.kts
+++ boogie-dafif/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-conventions")
+    id("com.vanniktech.maven.publish.base") version "0.34.0"
 }
 
 dependencies {
@@ -8,4 +9,45 @@ dependencies {
 
     testImplementation(platform(rootProject.libs.junit.bom))
     testImplementation(rootProject.libs.bundles.test.tools)
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    val releaseVersion = System.getenv("BOOGIE_RELEASE_VERSION")
+    version = if (releaseVersion.isNullOrBlank()) project.version.toString() else releaseVersion
+
+    coordinates("org.mitre.boogie", project.name, version.toString())
+
+    pom {
+        name.set(project.name)
+        description.set(rootProject.property("description").toString())
+        inceptionYear.set(rootProject.property("inceptionYear").toString())
+        url.set(rootProject.property("url").toString())
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("dbaker-mitre")
+                name.set("David Baker")
+                url.set("https://github.com/dbaker-mitre")
+            }
+            developer {
+                id.set("mattpollock")
+                name.set("Matt Pollock")
+                url.set("https://github.com/mattpollock")
+            }
+        }
+        scm {
+            url.set(rootProject.property("url").toString())
+            connection.set(rootProject.property("connection").toString())
+            developerConnection.set(rootProject.property("developerConnection").toString())
+        }
+    }
 }
