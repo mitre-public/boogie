d892a861d9283171c66088d98499be30b385fc14
David Baker <dbaker@mitre.org>
2025-10-20 14:55:27 -0400

    cleanup

--- build-logic/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        maven { name = "gradle-plugin-portal" }
-        maven { name = "caasd-plugin-portal" }
-    }
-    plugins {
-        id("caasd.repositories") version "3.1.1"
-    }
-}
