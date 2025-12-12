ec4b267e4a77586288a772d255eff94292e31f65
David Baker <dbaker@mitre.org>
2025-12-12 09:32:03 -0500

    integration tests

--- boogie-core/src/main/java/org/mitre/tdp/boogie/Airport.java
+++ boogie-core/src/main/java/org/mitre/tdp/boogie/Airport.java
@@ -5,7 +5,6 @@ import static java.util.Optional.ofNullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
