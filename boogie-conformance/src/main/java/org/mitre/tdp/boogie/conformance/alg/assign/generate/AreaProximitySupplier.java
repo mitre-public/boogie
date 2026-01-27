bd981f9c0319e4d8aae91040d59f168f53d9058e
David Baker <dbaker@mitre.org>
2026-01-27 09:31:37 -0500

    Creates the AreaProxmitySupplier which is a general case for the old StarToApproach leg

--- boogie-conformance/src/main/java/org/mitre/tdp/boogie/conformance/alg/assign/generate/AreaProximitySupplier.java
+++ boogie-conformance/src/main/java/org/mitre/tdp/boogie/conformance/alg/assign/generate/AreaProximitySupplier.java
@@ -1,12 +1,24 @@
 package org.mitre.tdp.boogie.conformance.alg.assign.generate;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import org.mitre.caasd.commons.LatLong;
+import org.mitre.caasd.commons.Pair;
+import org.mitre.tdp.boogie.Fix;
+import org.mitre.tdp.boogie.Leg;
 import org.mitre.tdp.boogie.conformance.alg.assign.FlyableLeg;
 import org.mitre.tdp.boogie.conformance.alg.assign.Route;
+import org.mitre.tdp.boogie.util.Combinatorics;
 
 public final class AreaProximitySupplier implements Supplier<Optional<FlyableLeg>> {
+
+  private static final Predicate<Pair<Leg, Leg>> FIX_EQUAL_AND_THERE = p -> p.first().associatedFix().equals(p.second().associatedFix())
+      && (p.first().associatedFix().isPresent() || p.second().associatedFix().isPresent());
+
   private final Route left;
   private final Route right;
 
@@ -17,6 +29,34 @@ public final class AreaProximitySupplier implements Supplier<Optional<FlyableLeg
 
   @Override
   public Optional<FlyableLeg> get() {
+    Collection<Pair<Leg, Leg>> pairs = Combinatorics.cartesianProduct(left.legs(), right.legs());
+    if (pairs.stream().anyMatch(FIX_EQUAL_AND_THERE)) {
+      return Optional.empty();
+    }
+
+    Optional<Leg> last = left.legs().stream()
+        .filter(i -> i.associatedFix().isPresent())
+        .reduce((f,s) -> s);
+    Optional<Leg> first = right.legs().stream()
+        .filter(i -> i.associatedFix().isPresent())
+        .findFirst();
+
+    if (last.isPresent() && first.isPresent()) {
+      double distance = last.get().associatedFix().get().latLong().distanceInNM(first.get().associatedFix().get().latLong());
+      LatLong mid = LatLong.quickAvgLatLong(last.get().associatedFix().get().latLong(), first.get().associatedFix().get().latLong());
+      AreaProximity areaProximity = new AreaProximity(mid, distance);
+      Fix fix = Fix.builder()
+          .latLong(mid)
+          .fixIdentifier("PROXIMITY")
+          .magneticVariation(first.get().associatedFix().get().magneticVariation().orElseThrow())
+          .build();
+      Leg standard = Leg.ifBuilder(fix, 0).build();
+      Leg record = Leg.record(areaProximity, standard);
+      Route route = Route.newRoute(List.of(record), areaProximity);
+      FlyableLeg flyableLeg = new FlyableLeg(null, record, null, route);
+      return Optional.of(flyableLeg);
+    }
+
     return Optional.empty();
   }
 }
