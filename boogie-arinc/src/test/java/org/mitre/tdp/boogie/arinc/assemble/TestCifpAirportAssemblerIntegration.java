9101f20c1e26edc9595d74aaa813cf83c88f92ea
David Baker <dbaker@mitre.org>
2026-08-24 10:37:49 -0400

    noticed it was disabled and fixed it.

--- boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/assemble/TestCifpAirportAssemblerIntegration.java
+++ boogie-arinc/src/test/java/org/mitre/tdp/boogie/arinc/assemble/TestCifpAirportAssemblerIntegration.java
@@ -1,20 +1,6 @@
 package org.mitre.tdp.boogie.arinc.assemble;
 
-import static java.util.stream.Collectors.toMap;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.regex.MatchResult;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mitre.caasd.commons.Pair;
@@ -25,6 +11,18 @@ import org.mitre.tdp.boogie.arinc.database.ArincDatabaseFactory;
 import org.mitre.tdp.boogie.arinc.database.ArincTerminalAreaDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.nonNull;
+import static java.util.stream.Collectors.toMap;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("CIFP")
 @Tag("INTEGRATION")
@@ -78,7 +76,6 @@ class TestCifpAirportAssemblerIntegration {
    * for now.
    */
   @Test
-  @Disabled
   void testUnpairedRunwayCountIsLow() {
 
     long allRunways = airports.values().stream()
@@ -100,66 +97,11 @@ class TestCifpAirportAssemblerIntegration {
     assertEquals(0., unpairedRatio, .015, "Ratio of runways without a reciprocal pair should be less than 1.5%.");
   }
 
-  private List<Pair<Runway, Runway>> pairsAt(Airport airport) {
-
-    Map<String, Runway> byReciprocal = airport.runways().stream()
-        .flatMap(r -> RunwayIdExtractor.invert(r.runwayIdentifier())
-            .stream()
-            .map(num -> Pair.of(num, r)))
-        .collect(toMap(Pair::first, Pair::second));
-
+  List<Pair<Runway, Runway>> pairsAt(Airport airport) {
+    Map<String, Runway> map = airport.runways().stream().collect(toMap(Runway::runwayIdentifier, Function.identity()));
     return airport.runways().stream()
-        .flatMap(r -> RunwayIdExtractor.normalize(r.runwayIdentifier())
-            .map(byReciprocal::get)
-            .map(o -> Pair.of((Runway) r, o))
-            .stream())
+        .map(r -> Pair.<Runway, Runway>of(r, ReciprocalRunwayIdentifier.INSTANCE.apply(r.runwayIdentifier()).map(map::get).orElse(null)))
+        .filter(p -> nonNull(p.second()))
         .collect(Collectors.toList());
-  }
-
-  /**
-   * Singleton class for extracting the numeric portion of a runway identifier.
-   */
-  private static final class RunwayIdExtractor {
-
-    private static final Pattern RUNWAY_NUMBER = Pattern.compile("(0[1-9]|[1-2]\\d|3[0-6])");
-
-    private static final Pattern PARALLEL_DESIGNATOR = Pattern.compile("[LRC]");
-
-    /**
-     * Simple regex for common runway ids (00-36).
-     */
-    public static Optional<String> runwayNumber(String runwayId) {
-      return RUNWAY_NUMBER.matcher(runwayId).results().findFirst().map(MatchResult::group);
-    }
-
-    public static Optional<String> parallelDesignator(String runwayId) {
-      return PARALLEL_DESIGNATOR.matcher(String.valueOf(runwayId.charAt(runwayId.length() - 1))).results().findFirst().map(MatchResult::group);
-    }
-
-    public static Optional<String> normalize(String runwayId) {
-      return runwayNumber(runwayId).flatMap(id -> parallelDesignator(runwayId).map(id::concat));
-    }
-
-    public static Optional<String> invert(String runwayId) {
-      return normalize(runwayId).map(id -> invertNumber(id).concat(invertDesignator(id)));
-    }
-
-    private static String invertDesignator(String runwayId) {
-      if (runwayId.length() == 3) {
-        char designator = runwayId.charAt(2);
-        if ('L' == designator) {
-          return "R";
-        } else if ('R' == designator) {
-          return "L";
-        } else {
-          return runwayId.substring(2, 3);
-        }
-      }
-      return "";
-    }
-
-    private static String invertNumber(String id) {
-      return String.format("%02d", (Integer.parseInt(id.substring(0, 2) + 18) % 36));
-    }
   }
 }
