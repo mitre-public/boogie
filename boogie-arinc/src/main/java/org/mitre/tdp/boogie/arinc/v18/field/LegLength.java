0134f0eb817f2d157a44e5d023a37afda152c58d
David Baker <dbaker@mitre.org>
2026-08-26 13:07:59 -0400

    holding was impacted also

--- boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/LegLength.java
+++ boogie-arinc/src/main/java/org/mitre/tdp/boogie/arinc/v18/field/LegLength.java
@@ -1,7 +1,19 @@
 package org.mitre.tdp.boogie.arinc.v18.field;
 
 import org.mitre.tdp.boogie.arinc.ArincDouble;
+import org.mitre.tdp.boogie.arinc.utils.ArincDecimalParser;
 
+import java.util.Optional;
+
+/**
+ * The Leg Length field specifies the distance of either the
+ * inbound leg or the outbound leg of the holding pattern. The determination of inbound
+ * or outbound is identified by the content of Section 5.298 of the applicable record.
+ * Inbound is defined as the distance between the point at which the aircraft rolls out
+ * on the inbound leg of the holding pattern and the fix at which the holding pattern is
+ * defined. Outbound is defined as the distance from a point abeam the holding fix to
+ * the beginning of the inbound turn (Figure 5-4).
+ */
 public final class LegLength extends ArincDouble {
 
   @Override
@@ -12,5 +24,12 @@ public final class LegLength extends ArincDouble {
   @Override
   public String fieldCode() {
     return "5.64";
+  }
+
+  @Override
+  public Optional<Double> apply(String fieldValue) {
+    return Optional.of(fieldValue)
+        .map(String::trim)
+        .flatMap(ArincDecimalParser.INSTANCE::parseDoubleWithTenths);
   }
 }
